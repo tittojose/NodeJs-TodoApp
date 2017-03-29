212495c86ec6180ff4cf16a82f84bddd54157235
Jose <mailtitto@gmail.com>
2017-03-30 00:01:24 +0530

    initialize todo app

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.listen(3000);
+
+console.log('You are listening to port 3000');
+
