a546db42bed595e7108a6d867064d8ea409a17b8
Jose <mailtitto@gmail.com>
2017-03-30 01:17:05 +0530

    todo landing page.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var todoController = require('./controllers/todoController');
 
 var app = express();
 
@@ -6,6 +7,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 
+// fire controllers
+todoController(app);
+
+// lister to port 3000
 app.listen(3000);
 
 console.log('You are listening to port 3000');
