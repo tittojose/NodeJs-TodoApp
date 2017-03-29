40a7a7b556c3c4513880c4df254466bf6d0c4ec5
Jose <mailtitto@gmail.com>
2017-03-30 01:48:22 +0530

    Adding data to todo list

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,23 @@
+var bodyParser = require('body-parser');
+
+var data = [
+    { item: 'learn node.' },
+    { item: 'auth 2.0 for todo.' },
+    { item: 'host on app engine.' }
+]
+
+var urlEncoder = bodyParser.urlencoded({ extended: false });
+
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo');
+        res.render('todo', { todos: data });
     });
 
-    app.post('/todo', function (req, res) {
-
+    app.post('/todo', urlEncoder, function (req, res) {
+        console.log(req.body);
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', function (req, res) {
---
+++ public/assets/todolist.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    $('#bAddToDo').on('click', function () {
+
+        var item = $('#todoInput');
+        var todo = { item: item.val() };
+        console.log(todo);
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success: function (data) {
+                //do something with the data via front-end framework
+                location.reload();
+            }
+        });
+
+        return false;
+
+    });
+
+    $('li').on('click', function () {
+        var item = $(this).text().replace(/ /g, "-");
+        $.ajax({
+            type: 'DELETE',
+            url: '/todo/' + item,
+            success: function (data) {
+                //do something with the data via front-end framework
+                location.reload();
+            }
+        });
+    });
+
+});
