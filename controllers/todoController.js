72cf87c4cb9fd6bdb3e0e1b0ae243e0ab0ab49db
Jose <mailtitto@gmail.com>
2017-03-30 20:07:37 +0530

    Mongoose db integration for todolist.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,77 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [
-    { item: 'learn node.' },
-    { item: 'auth 2.0 for todo.' },
-    { item: 'host on app engine.' }
-]
+// connect to db
+
+mongoose.connect('mongodb://test:test@ds145790.mlab.com:45790/tododb');
+
+// create scheme of mongoose db
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var itemOne = Todo({
+//     item: 'First todo'
+// }).save(function (err) {
+//     if (err) throw err;
+
+//     console.log('item save');
+// });
+
+
+// var data = [
+//     { item: 'learn node.' },
+//     { item: 'auth 2.0 for todo.' },
+//     { item: 'host on app engine.' }
+// ]
 
 var urlEncoder = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo', { todos: data });
+        // get data mongo db
+        Todo.find({}, function (err, data) {
+            if (err) {
+                throw err;
+            } else {
+                res.render('todo', { todos: data });
+            }
+        });
+
     });
 
     app.post('/todo', urlEncoder, function (req, res) {
         console.log(req.body);
-        data.push(req.body);
-        res.json(data);
+        var newTodo = Todo(req.body).save(function (err, data) {
+            if (err) {
+                throw err;
+            } else {
+                res.json({});
+            }
+        });
+
+
     });
 
-    app.delete('/todo', function (req, res) {
+    app.delete('/todo/:item', function (req, res) {
+        console.log(req.params.item);
+
+        Todo.find({ item: req.params.item.replace(/\-/g, '') })
+            .remove(function (err, data) {
+                if (err) {
+                    throw err;
+                } else {
+                    res.json({});
+                }
+            });
+
+        // data = data.filter(function (todo) {
+        //     return todo.item.replace(/ /g, "-") !== req.params.item;
+        // });
 
     });
 
--- public/assets/todolist.js
+++ public/assets/todolist.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
     });
 
     $('li').on('click', function () {
-        var item = $(this).text().replace(/ /g, "-");
+        var item = $(this).text().trim();
+        console.log(item);
         $.ajax({
             type: 'DELETE',
             url: '/todo/' + item,
